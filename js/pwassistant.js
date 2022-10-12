713d37a8c2ddc668c70bc470721c9ecce7325f58
Jean-Olivier LeSieur <LESJEA01@reg04.rtss.qc.ca>
2022-10-12 09:20:34 -0400

    Added more actions

--- js/pwassistant.js
+++ js/pwassistant.js
@@ -1,3 +1,6 @@
+////////////////////////////////////////
+// Début Script Menu Navigation
+////////////////////////////////////////
 $(".navbar-nav .nav-item").on("click", function () {
     $(".navbar-nav .nav-item").find(".active").removeClass("active");
 });
@@ -8,12 +11,24 @@ $(".navbar-nav li a").on("click", function () {
     }
 });
 
+// Set active sub-tab name to dropmenu text.
+$(".sub-tab a").on("click", function () {
+    $(this).closest("li").find("span").text($(this).text());
+});
+
+////////////////////////////////////////
+// Fin Script Menu Navigation
+////////////////////////////////////////
+
 var dataSet = [];
 var arr = null;
 var table;
+var currentRow = null;
+var localisation = null;
 
+// Function to be run when the page load.
 $(document).ready(function () {
-    $("#formControlSelect1").focus();
+    $("#selectIntervention").focus();
 
     table = $('#example').DataTable({
         order: [0, 'desc'],
@@ -21,12 +36,22 @@ $(document).ready(function () {
         data: dataSet
     });
 
+    $('#example tbody').on('click', 'tr', function () {
+        if ($(this).hasClass('selected')) {
+            $(this).removeClass('selected');
+            currentRow = null;
+        } else {
+            table.$('tr.selected').removeClass('selected');
+            $(this).addClass('selected');
+            currentRow = $(this).closest("tr");
+        }
+    });
+
     $(this).one('keydown', function (e) {
         if (e.keyCode == 17) {
             $(".btn-delete").removeAttr("disabled");
         }
     });
-
 
     $('#addPumpform input').keyup(function () {
         var empty = false;
@@ -44,43 +69,57 @@ $(document).ready(function () {
     });
 });
 
-$('#newTag, #newSerial').keyup(function () {
-    $(this).val($(this).val().toUpperCase());
-});
+////////////////////////////////////////
+// Début Script Page Intervention
+////////////////////////////////////////
+$('a[href="#content-2"]').on("shown.bs.tab", function (e) {
+    $("#selectIntervention").focus();
+})
 
-$('body').on('keyup', function (e) {
-    if (e.keyCode == 17) {
-        //do something
-        $(".btn-delete").attr("disabled", "disabled");
-    }
-    // after first keyup set to handle next keydown only once:
-    $(this).one('keydown', function (e) {
-        if (e.keyCode == 17) {
-            $(".btn-delete").removeAttr("disabled");
-        }
-    });
-});
-
-$(function () {
-    var list = [
-               "CHAUR",
-               "Shawinigan",
-               "Maskinongé",
-               "BNY",
-                "Prêt d'équipements"
-            ];
-    $("#newLocalisation").autocomplete({
-        source: list
-    });
-});
-
-$("#formControlSelect1").focusout(function () {
-    var barcode = $("#formControlSelect1 option:selected").val();
+$("#selectIntervention").focusout(function () {
+    var action = $("#selectIntervention option:selected").val();
     //console.log(barcode);
 
-    if (barcode == "") {
-        $("#formControlSelect1").focus();
+    if (action == "") {
+        $("#selectIntervention").focus();
         return false;
+    }
+});
+
+$("#serial").on('input', function () {
+    if ($(this).val().length) {
+        // textbox not empty
+        if ($(this).val().length == 27 || $(this).val().length == 9) {
+            $(".btn-valider-intervention").removeAttr("disabled");
+        } else {
+            $(".btn-valider-intervention").attr('disabled', 'disabled');
+        }
+        // Try to retreive Tag Name from serial number
+        var testIt = $(this).val();
+        var i = 0,
+            j = 0,
+            indx = [],
+            msg;
+        for (i = 0; i < arr.length; i++) {
+            for (j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] === testIt) {
+                    indx = [i, j];
+                    break;
+                }
+            }
+        }
+        if (typeof indx[0] == "undefined" || typeof indx[1] == "undefined") {
+            $("#tag").val('');
+            localisation = "N/A";
+        } else {
+            $("#tag").val(arr[indx[0]][1]);
+            localisation = arr[indx[0]][0];
+        }
+    } else {
+        // textbox empty
+        $("#tag").val('');
+        $("#tag").prop('selected', false);
+        $(".btn-valider-intervention").attr('disabled', 'disabled');
     }
 });
 
@@ -88,9 +127,8 @@ $("#serial").keypress(function (e) {
     if (e.which == 13) {
         e.preventDefault();
         var serialNumber = $("#serial").val();
-        console.log(serialNumber.length);
-        console.log(arr);
-
+        //console.log(serialNumber.length);
+        //console.log(arr);
 
         if (serialNumber.length == 27 || serialNumber.length == 9) {
             serialNumber = serialNumber.substr(serialNumber.length - 9);
@@ -106,44 +144,90 @@ $("#serial").keypress(function (e) {
             return;
         }
 
-        var testIt = serialNumber;
-        var i = 0,
-            k = 0,
-            indx = [],
-            msg;
-        for (i = 0; i < arr.length; i++) {
-            for (k = 0; k < arr[i].length; k++) {
-                if (arr[i][k] === testIt) {
-                    indx = [i, k];
-                    break;
-                }
-            }
+        /*var testIt = serialNumber;
+var i = 0,
+    k = 0,
+    indx = [],
+    msg;
+for (i = 0; i < arr.length; i++) {
+    for (k = 0; k < arr[i].length; k++) {
+        if (arr[i][k] === testIt) {
+            indx = [i, k];
+            break;
         }
-        if (typeof indx[0] == "undefined" || typeof indx[1] == "undefined") {
-            msg = ("N/A");
-            $("#tag").val(msg);
-        } else {
-            msg = "i= " + indx[0] + " k= " + indx[1];
-            $("#tag").val(arr[indx[0]][1]);
-        }
-        console.log(msg);
+    }
+}
+if (typeof indx[0] == "undefined" || typeof indx[1] == "undefined") {
+    $("#tag").val("N/A");
+    localisation = "N/A";
+} else {
+    $("#tag").val(arr[indx[0]][1]);
+    localisation = arr[indx[0]][0];
+}*/
 
-        var action = $("#formControlSelect1 option:selected").val();
-        console.log(action);
+        var action = $("#selectIntervention option:selected").val();
         if (action == "FIN") {
-            //$(".btn-valider-intervention").removeAttr("disabled");
+            saveData();
+        }
+        if (action == "REPAR") {
+            $("#note").focus();
+        }
+        if (action == "REPARKCI" || action == "LOCATION") {
+            $("#tracking").focus();
+        }
+    }
+});
+
+$("#tracking").keypress(function (e) {
+    if (e.which == 13) {
+        e.preventDefault();
+        var tracking = $("#tracking").val();
+
+        // Tracking PUROLATOR
+        if (tracking.length == 34) {
+            $("#tracking").val(tracking.substring(11, 23));
+        } else {
+            $("#tracking").val("");
+            return;
+        }
+
+        var action = $("#selectIntervention option:selected").val();
+        if (action == "REPARKCI") {
+            $("#note").focus();
+        }
+        if (action == "LOCATION") {
             saveData();
         }
     }
 });
 
-$("#formControlSelect1").change(function () {
-    var barcode = $("#formControlSelect1 option:selected").val();
-    //console.log(barcode);
+$("#selectIntervention").change(function () {
+    var action = $("#selectIntervention option:selected").val();
+    console.log(action);
 
-    if (barcode != "") {
+    if (action == "FIN") {
         $("#serial").prop("disabled", false);
         $("#tag").prop("disabled", false);
+        $("#tracking").prop("disabled", true);
+        $("#note").prop("disabled", true);
+    }
+    if (action == "REPAR") {
+        $("#serial").prop("disabled", false);
+        $("#tag").prop("disabled", false);
+        $("#tracking").prop("disabled", true);
+        $("#note").prop("disabled", false);
+    }
+    if (action == "REPARKCI") {
+        $("#serial").prop("disabled", false);
+        $("#tag").prop("disabled", false);
+        $("#tracking").prop("disabled", false);
+        $("#note").prop("disabled", false);
+    }
+    if (action == "LOCATION") {
+        $("#serial").prop("disabled", false);
+        $("#tag").prop("disabled", true);
+        $("#tracking").prop("disabled", false);
+        $("#note").prop("disabled", true);
     }
 });
 
@@ -165,70 +249,90 @@ $("#tag").change(function () {
             }
         }
         if (typeof indx[0] == "undefined" || typeof indx[1] == "undefined") {
-            msg = ("");
-            $("#serial").val(msg);
+            $("#serial").val("");
+            localisation = "N/A";
         } else {
-            msg = "i= " + indx[0] + " k= " + indx[1];
             $("#serial").val(arr[indx[0]][2]);
             $(".btn-valider-intervention").removeAttr("disabled");
+            localisation = arr[indx[0]][0];
         }
     }
 });
 
-
-$('a[href="#content-2"]').on("shown.bs.tab", function (e) {
-    $("#formControlSelect1").focus();
-})
-
-$(".drop").click(function () {
-    if ($(this).closest("form").find(".drop_content").hasClass("d-none")) {
-        $(this).parents(".root").find(".drop_content").addClass("d-none");
-        $(this).parents(".root").find(".drop svg").removeClass('fa-chevron-up').addClass('fa-chevron-down');
-        $(this).closest("form").find(".drop_content").removeClass("d-none");
-        $(this).find("svg").removeClass('fa-chevron-down').addClass('fa-chevron-up');
-    } else {
-        $(this).closest("form").find(".drop_content").addClass("d-none");
-        $(this).find("svg").removeClass('fa-chevron-up').addClass('fa-chevron-down');
+function populateDropdown() {
+    arr = json_obj["pompeVAC"]["inventaire"];
+    // Populate dropdown
+    var dropMenu = document.getElementById("tag");
+    for (var i = 0; i < arr.length; i++) {
+        var opt = arr[i][1];
+        var el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        dropMenu.appendChild(el);
     }
-    $(this).parents(".root").find(".charbon-dt").addClass("d-none");
+};
+////////////////////////////////////////
+// Fin Script Page Intervention
+////////////////////////////////////////
 
-    $('body,html').animate({
-        scrollTop: 0
-    }, 0);
+////////////////////////////////////////
+// Début Script Page Historique
+//
+// Référence https://datatables.net/
+////////////////////////////////////////
+// Watch "CTRL" keypress to toggle "delete" button visibility.
+$('body').on('keyup', function (e) {
+    if (e.keyCode == 17) {
+        //do something
+        $(".btn-delete").attr("disabled", "disabled");
+    }
+    // After first keyup set to handle next keydown only once.
+    $(this).one('keydown', function (e) {
+        if (e.keyCode == 17) {
+            $(".btn-delete").removeAttr("disabled");
+        }
+    });
 });
 
-Date.prototype.toDateInputValue = (function () {
-    var local = new Date(this);
-    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
-    return local.toJSON().slice(0, 10);
-});
-
-
-$("input[type=checkbox]").change(function () {
-    var senderRootID = $(this).parents(".root").attr("id");
-    var senderFormID = $(this).closest("form").attr("id");
-
-    var checked = $("#" + senderFormID + " input:checked").length > 0;
-    if (checked) {
-        $("#" + senderRootID + " .btn-success").removeAttr("disabled");
-    } else {
-        $("#" + senderRootID + " .btn-success").attr("disabled", "disabled");
+$('.btn-delete').click(function () {
+    //table.row('.selected').remove().draw(false);
+    if (currentRow != null) {
+        var data = $('#example').DataTable().row(currentRow).data();
+        console.log(data[0]);
     }
 });
 
-// Set active sub-tab name to dropmenu text.
-$(".sub-tab a").on("click", function () {
-    $(this).closest("li").find("span").text($(this).text());
+function loadTable() {
+    dataSet = json_obj["pompeVAC"]["historique"];
+
+    // Create form, order by ID and hide the first column.
+    table.clear().rows.add(dataSet).columns('.id').order('desc').columns([0]).visible(false).draw();
+};
+////////////////////////////////////////
+// Fin Script Page Historique
+////////////////////////////////////////
+
+////////////////////////////////////////
+// Début Script Page Autre
+////////////////////////////////////////
+
+// Force l'entrée des données en majuscules
+$('#newTag, #newSerial').keyup(function () {
+    $(this).val($(this).val().toUpperCase());
 });
 
+// Indices pour la localisation de la pompe
 $(function () {
-    $(document).on("click", ".alert-close", function () {
-        $(this).parent().hide();
-    })
-});
-
-$(".btn-valider-intervention").on("click", function () {
-    saveData();
+    var list = [
+               "CHAUR",
+               "Shawinigan",
+               "Maskinongé",
+               "BNY",
+                "Prêt d'équipements"
+            ];
+    $("#newLocalisation").autocomplete({
+        source: list
+    });
 });
 
 $(".btn-valider-addPump").on("click", function () {
@@ -257,6 +361,7 @@ $(".btn-valider-addPump").on("click", function () {
             document.getElementById("success-alert").classList.remove("d-none");
         }
 
+        // Reset form entries
         $(':input', '#addPumpform')
             .not(':button, :submit, :reset, :hidden')
             .val('')
@@ -273,6 +378,19 @@ $(".btn-valider-addPump").on("click", function () {
         }, 5000);
     })
 });
+////////////////////////////////////////
+// Fin Script Page Autre
+////////////////////////////////////////
+
+$(function () {
+    $(document).on("click", ".alert-close", function () {
+        $(this).parent().hide();
+    })
+});
+
+$(".btn-valider-intervention").on("click", function () {
+    saveData();
+});
 
 function saveData() {
     var currentDate = new Date();
@@ -284,20 +402,20 @@ function saveData() {
         ('0' + (currentDate.getMonth() + 1)).slice(-2) + '-' +
         currentDate.getFullYear();
 
-    var intervention = $("#formControlSelect1 option:selected").text().substring(3);
+    var intervention = $("#selectIntervention option:selected").text().substring(3);
     var serial = $("#serial").val();
     var tag = $("#tag").val();
     var tracking = $("#tracking").val();
     var note = $("#note").val();
 
-    newData = [entryID, currentDateString, intervention, tag, serial, "CHAUR", tracking, note];
+    newData = [entryID, currentDateString, intervention, tag, serial, localisation, tracking, note];
     json_obj["pompeVAC"]["historique"].push(newData);
     //console.log(JSON.stringify(json_obj["pompeVAC"]["historique"]));
 
     // Get a new write batch
     var batch = db.batch();
 
-    // Set the value of 'hemodialyse -> inspection'
+    // Set the value of 'pompeVAC -> historique'
     var hemoRef = db.collection("pompeVAC").doc("historique");
     batch.set(hemoRef, {
         data: JSON.stringify(json_obj["pompeVAC"]["historique"])
@@ -318,13 +436,13 @@ function saveData() {
             .prop('checked', false)
             .prop("disabled", true);
 
-        $('#formControlSelect1')
+        $('#selectIntervention')
             .prop('selected', false)
             .prop("disabled", false);
 
         $(".btn-valider-intervention").attr("disabled", "disabled");
 
-        $("#formControlSelect1").focus();
+        $("#selectIntervention").focus();
 
         $('body,html').animate({
             scrollTop: 0
@@ -335,33 +453,4 @@ function saveData() {
             //document.getElementById('success-alert').classList.add('d-none');
         }, 5000);
     })
-
-};
-
-function populateDropdown() {
-    arr = json_obj["pompeVAC"]["inventaire"];
-    // Populate dropdown
-    var dropMenu = document.getElementById("tag");
-    for (var i = 0; i < arr.length; i++) {
-        var opt = arr[i][1];
-        var el = document.createElement("option");
-        el.textContent = opt;
-        el.value = opt;
-        dropMenu.appendChild(el);
-    }
-};
-
-function loadTable() {
-
-    //console.log("historique: ", json_obj["pompeVAC"]["historique"]);
-
-
-    dataSet = json_obj["pompeVAC"]["historique"];
-    //console.log(dataSet);
-
-    table.clear().rows.add(dataSet).draw().columns('.id').order('desc').draw().columns([0]).visible(false);
-
-    // Toggle the visibility
-    //var column = table.column(1);
-    //column.visible(!column.visible());
 };
